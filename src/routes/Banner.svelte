023fb9457e13f1bb7434ec712ea19ec4cba20e84
a-nguyen-kangan <a.nguyen@kangan.edu.au>
2023-09-06 12:23:06 +1000

    inputs and add button

---
+++ src/routes/Banner.svelte
@@ -0,0 +1,1 @@
+<h1>MotorCycle List App</h1>
---
+++ src/routes/Input.svelte
@@ -0,0 +1,27 @@
+<script>
+let brand;
+let model = '';
+let seller = '';
+let price = '';
+
+let display = false;
+
+function displayValues() {
+    display = true;
+}
+</script>
+
+<input type="text" id="brand" bind:value={brand} placeholder="brand"/>
+<input type="text" id="model" bind:value={model} placeholder="model"/>
+<input type="text" id="seller" bind:value={seller} placeholder="seller"/>
+<input type="number" id="price" bind:value={price} placeholder="price"/>
+
+{#if brand && model && seller && price}
+<button id="add" on:click={() => displayValues()}>Add</button>
+{/if}
+
+<hr>
+
+{#if display}
+    {brand} {model} {seller} {price}
+{/if}
